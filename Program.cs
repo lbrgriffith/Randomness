bb43e72c8eb41791037449df6fe9e98e1b716613
Ricardo Griffith <ricardo.griffith@icloud.com>
2020-09-04 16:27:16 -0300

    Added Random number generation method and loop for displaying increasing random number calls to the new method.

--- Program.cs
+++ Program.cs
@@ -8,8 +8,21 @@ namespace Randomness
 {
     class Program
     {
+        //Function to get random number
+        static readonly Random getrandom = new Random();
+
         static void Main(string[] args)
         {
+            for (var counter = 1; counter < 10; counter++)
+                Console.WriteLine($"Random Number: {RandomNumber(counter, counter+10):N0}");
+        }
+
+        static int RandomNumber(int min, int max)
+        {
+            lock (getrandom) // synchronize
+            {
+                return getrandom.Next(min, max);
+            }
         }
     }
 }
