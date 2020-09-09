163c83ef3f710481ab40215bbb81ab3358f5fd20
Ricardo Griffith <ricardo.griffith@icloud.com>
2020-09-09 18:18:08 -0300

    Added Random Text method and code to display on screen.

--- Program.cs
+++ Program.cs
@@ -1,16 +1,20 @@
 ﻿using System;
+using System.Linq;
 
 namespace Randomness
 {
     class Program
     {
         //Function to get random number
-        static readonly Random getrandom = new Random();
+        static readonly Random random = new Random();
 
         static void Main(string[] args)
         {
             for (var counter = 1; counter < 10; counter++)
                 Console.WriteLine($"Random Number (between {counter},{counter + 10}): {RandomNumber(counter, counter+10):N0}");
+
+            for (var counter = 1; counter < 10; counter++)
+                Console.WriteLine($"Random Text (length: {counter}) => {RandomText(counter)}");
 
             Console.WriteLine("\nPress <Enter> to exit.");
             Console.ReadLine();
@@ -26,11 +30,28 @@ namespace Randomness
         /// <returns>Returns a random integer within the specified range.</returns>
         static int RandomNumber(int min, int max)
         {
-            lock (getrandom) // synchronize
+            lock (random) // synchronize
             {
-                return getrandom.Next(min, max);
+                return random.Next(min, max);
             }
-        } 
+        }
+
+        #endregion
+
+        #region Text
+
+        /// <summary>
+        /// Generates a random alpha-numeric string based on the length specified.
+        /// </summary>
+        /// <param name="length">Length of alpha-numeric string to generate.</param>
+        /// <returns></returns>
+        static string RandomText(int length)
+        {
+            const string chars = 
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new String(Enumerable.Repeat(chars, length)
+              .Select(sub => sub[random.Next(sub.Length)]).ToArray());
+        }
 
         #endregion
     }
